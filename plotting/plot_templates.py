4e622c9ac23f50e2418848c83e4d66f46ad6db6c
joanba68 <joan.altadill.elias@gmail.com>
2025-07-11 10:05:11 +0200

    Initial tests

--- plotting/plot_templates.py
+++ plotting/plot_templates.py
@@ -2,6 +2,7 @@ from plotting.plot_utils import plot_df
 from plotting.config import AxisConfig, CommonPlotConfig, PlotConfig
 from metrics_extractor.metrics_io import load_metrics
 
+import pandas as pd
 
 def tps_players_plot(experiment, selected_metrics, type_exp):
     """
@@ -444,3 +445,338 @@ def players_chunks_owner_plot(experiment, selected_metrics, type_exp):
     )
 
     plot_df(dfs_by_server, None, conf)
+
+def quality_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot Quality ratio per server.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+    
+    quality = load_metrics(selected_metrics[32], experiment, type_exp) # (veloctiy_server_quality)
+
+    servers = sorted(quality["exported_server_name"].dropna().astype(str).unique())
+
+    dfs_by_server = [
+        quality[quality["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    primary_axis = AxisConfig(
+        labels=[f"Server {i+1}" for i in range(len(servers))],
+        ylabel="Quality",
+        plot_kwargs=[
+            {"linestyle": "-"} for _ in servers
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="Quality ratio by Server",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "best"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/quality_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+    )
+
+    plot_df(dfs_by_server, None, conf)
+
+def players_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot Quality ratio per server.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+    
+    players = load_metrics(selected_metrics[31], experiment, type_exp) # (veloctiy_server_players)
+
+    servers = sorted(players["exported_server_name"].dropna().astype(str).unique())
+
+    dfs_by_server = [
+        players[players["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    primary_axis = AxisConfig(
+        labels=[f"Server {i+1}" for i in range(len(servers))],
+        ylabel="Players",
+        plot_kwargs=[
+            {"linestyle": "-"} for _ in servers
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="Num. players by Server",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "best"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/players_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+    )
+
+    plot_df(dfs_by_server, None, conf)
+
+def mspt_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot Quality ratio per server.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+    
+    mspt = load_metrics(selected_metrics[30], experiment, type_exp) # (veloctiy_server_players)
+
+    servers = sorted(mspt["exported_server_name"].dropna().astype(str).unique())
+
+    dfs_by_server = [
+        mspt[mspt["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    primary_axis = AxisConfig(
+        labels=[f"Server {i+1}" for i in range(len(servers))],
+        ylabel="MSPT",
+        plot_kwargs=[
+            {"linestyle": "-"} for _ in servers
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="MSPT by Server",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "best"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/mspt_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+    )
+
+    plot_df(dfs_by_server, None, conf)
+
+def mspt_tps_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot MSPT and TPS.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+
+    mspt = load_metrics(selected_metrics[30], experiment, type_exp) # (veloctiy_server_players)
+    average_tps = load_metrics(selected_metrics[25], experiment, type_exp)
+
+    # primary_axis = AxisConfig(
+    #     labels=["MSPT"],
+    #     ylabel="MSPT",
+    #     ylim=(0, None),
+    #     plot_kwargs=[
+    #         {"color": "blue", "linestyle": "-"}
+    #     ]
+    # )
+
+    servers = sorted(mspt["exported_server_name"].dropna().astype(str).unique())
+    dfs_by_server = [
+        mspt[mspt["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    primary_axis = AxisConfig(
+        labels=[f"Server {i+1}" for i in range(len(servers))],
+        ylabel="MSPT",
+        plot_kwargs=[
+            {"linestyle": "-"} for _ in servers
+        ]
+    )
+
+    secondary_axis = AxisConfig(
+        labels=["Average TPS"],
+        ylabel="Avg TPS",
+        ylim=(0, 20),
+        plot_kwargs=[
+            {"color": "orange", "linestyle": "-"}
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="MSPT and avg TPS",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "upper left"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        # grid_kwargs={"linestyle": "-"},
+        # minor_grid_kwargs={"linestyle": ":"},
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/mspt_tps_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+        secondary_axis=secondary_axis
+    )
+
+    #filter the data so that only timestamp and value are kept
+    average_tps = average_tps[["timestamp", "value"]]
+
+    plot_df( dfs_by_server, [average_tps], conf)
+
+def owned_chunks_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot Chunk Ownership by Server.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+
+    chunks = load_metrics(selected_metrics[29], experiment, type_exp) # (veloctiy_server_chunks)
+
+    servers = sorted(chunks["exported_server_name"].unique())
+    # Create a DataFrame for each server's players
+    dfs_by_server = [
+        chunks[chunks["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    # Calcular el màxim i el mínim de la diferència del nombre de chunks entre servers
+    combined_df = pd.concat(
+        [df.set_index("timestamp")["value"].rename(f"server_{i+1}") for i, df in enumerate(dfs_by_server)],
+        axis=1
+    )
+    # Calculate max-min difference
+    max_min_diff = abs(combined_df.max(axis=1) - combined_df.min(axis=1))
+    # Create a DataFrame for the difference
+    diff_df = pd.DataFrame({"timestamp": max_min_diff.index, "value": max_min_diff.values}).reset_index(drop=True)
+
+    # Cal ajustar els zeros de les escales del gràfic al mateix nivell
+    primary_max = max(df["value"].max() for df in dfs_by_server)
+    primary_min = min(df["value"].min() for df in dfs_by_server)
+    primary_ylim = (min(0, primary_min), primary_max * 1.1)
+
+    # Calcular els límits per al secondary_axis
+    secondary_max = diff_df["value"].max()
+    secondary_min = 0
+    secondary_ylim = (secondary_min, secondary_max * 1.1)
+
+    primary_axis = AxisConfig(
+        labels=[f"Chunks Server {i+1}" for i in range(len(servers))],
+        ylabel="Chunks Owned",
+        ylim = primary_ylim,
+        plot_kwargs=[
+            {"linestyle": "-"} for _ in servers
+        ]
+    )
+
+    secondary_axis = AxisConfig(
+        labels=["Max. difference of owned chunks between all servers"],
+        ylabel="Chunks",
+        ylim=secondary_ylim,
+        plot_kwargs=[
+            {"color": "black", "linestyle": "dashdot"}
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="Owned Chunks by Server",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "best"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/chunk_ownership_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+        secondary_axis=secondary_axis
+    )
+
+    plot_df(dfs_by_server, [diff_df], conf)
+
+def max_diff_chunks_master_plot(experiment, selected_metrics, type_exp):
+    """
+    Plot Chunk Ownership by Server.
+    :param experiment: The name of the experiment to plot.
+    :param selected_metrics: List of selected metrics.
+    :param type_exp: Type of experiment ('exp_vanilla' or 'exp_mod').
+    """
+
+    chunks = load_metrics(selected_metrics[29], experiment, type_exp) # (veloctiy_server_chunks)
+
+    servers = sorted(chunks["exported_server_name"].unique())
+    # Create a DataFrame for each server's players
+    dfs_by_server = [
+        chunks[chunks["exported_server_name"] == server][["timestamp", "value"]].reset_index(drop=True)
+        for server in servers
+    ]
+
+    # Calcular el màxim i el mínim de la diferència del nombre de chunks entre servers
+    combined_df = pd.concat(
+        [df.set_index("timestamp")["value"].rename(f"server_{i+1}") for i, df in enumerate(dfs_by_server)],
+        axis=1
+    )
+    # Calculate max-min difference
+    max_min_diff = abs(combined_df.max(axis=1) - combined_df.min(axis=1))
+    # Create a DataFrame for the difference
+    diff_df = pd.DataFrame({"timestamp": max_min_diff.index, "value": max_min_diff.values}).reset_index(drop=True)
+
+    primary_axis = AxisConfig(
+        labels=[f"Chunks Server {i+1}" for i in range(len(servers))],
+        ylabel="Chunks Owned",
+        plot_kwargs=[
+            {"color": "black"} for _ in servers
+        ]
+    )
+
+    common_conf = CommonPlotConfig(
+        title="Max. difference of owned chunks by server",
+        figsize=(15, 6),
+        show_legend=True,
+        legend_kwargs={"loc": "best"},
+        tight_layout=True,
+        grid=True,
+        grid_minor=False,
+        minor_ticks=False,
+        time_unit='s',
+        output_path=f"plots/{type_exp}/{experiment}/chunk_diff_{experiment}.pdf"
+    )
+
+    conf = PlotConfig(
+        common=common_conf,
+        primary_axis=primary_axis,
+    )
+
+    plot_df([diff_df], None, conf)
